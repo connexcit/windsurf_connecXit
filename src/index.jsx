c10493dc8047c5bc79c900e1c0fdee61865de3b2
tdaentbluv <tdaentbluv@gmail.com>
2025-01-10 13:24:27 -0500

    fix: remove non-existent color.css import
    - Removed color.css import as the file doesn't exist
    - Reordered CSS imports for better organization
    - Added React.StrictMode wrapper

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./styles/color.css";
+import "./styles/tailwind.css";
 import "./styles/font.css";
 import "./styles/index.css";
-import "./styles/tailwind.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
